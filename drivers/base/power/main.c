ecb33eb704309f062e3e43b4a7901f17dbe59608
Aditya Kevin <adityakevin50@gmail.com>
2019-08-12 02:33:56 +0000

    drivers: fix much error

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1355,10 +1355,8 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 
 	dpm_wait_for_children(dev, async);
 
-	if (async_error) {
-		dev->power.direct_complete = false;
+	if (async_error)
 		goto Complete;
-	}
 
 	/*
 	 * If a device configured to wake up the system from sleep states
@@ -1370,9 +1368,9 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 		pm_wakeup_event(dev, 0);
 
 	if (pm_wakeup_pending()) {
-
-		dev->power.direct_complete = false;
-
+		pm_get_active_wakeup_sources(suspend_abort,
+			MAX_SUSPEND_ABORT_LEN);
+		log_suspend_abort_reason(suspend_abort);
 		async_error = -EBUSY;
 		goto Complete;
 	}
